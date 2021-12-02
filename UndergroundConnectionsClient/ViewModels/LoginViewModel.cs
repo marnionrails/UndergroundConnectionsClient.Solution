cdd0f323ae0b22e5dbdfe262d82d69a2add30216
Gabriel Ayala <gabeayala100@gmail.com>
2021-12-01 18:59:20 -0800

    Add views and view models for the login/register
    Co-authored-by: Sisi Vieira <cicy886@gmail.com>
    Co-authored-by: Mary Marks <maryleemarks@gmail.com>

---
+++ UndergroundConnectionsClient/ViewModels/LoginViewModel.cs
@@ -0,0 +1,8 @@
+ namespace UndergroundConnectionsClient.ViewModels
+{
+  public class LoginViewModel
+    {
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+}
